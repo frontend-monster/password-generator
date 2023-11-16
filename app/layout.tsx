0c6ea781e5f979eac7457dfb350c2ff911ceb0e9
Arda Kaan Aydilek <developer@talconas.com>
2023-11-17 01:43:48 +0300

    remove modetoggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,6 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <ModeToggle />
           {children}
           <Toaster />
         </ThemeProvider>
